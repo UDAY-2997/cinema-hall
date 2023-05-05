21a41bf441c469e662d927ba5557a6ac5b85a6cf
U-Dev <udaythakur.shely@gmail.com>
2023-05-04 20:31:30 -0500

    funtions added

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,111 +1,113 @@
 "use strict";
 
-import { select, print } from "./utils.js";
+import { select, print, onEvent } from "./utils.js";
 
+const moviesInput = select(".search-movie");
+const cityInput = select(".search-city");
+let listmovies = select(".movie-completion");
+let listCities = select(".city-completion");
 
+moviesInput.value = "";
+cityInput.value = "";
 
-const search = document.getElementsByClassName("search");
-
-
- 
-const movieNames = [
-  "Jurassic Park","Eternals","The Matrix","JFK",
-  "The Lord of the Rings: The Fellowship of the Ring",
-  "No Time to Die","The Batman","Rocky","The Exorcist",
-  "West Side Story","The Lion King","Gone in 60 Seconds",
-  "Titanic","The Godfather","The Remains of the Day"
-]
-
-// const movieNames = cities.results[0].title
-
-const movieNamesSort = movieNames.sort();
-console.log(movieNamesSort);
-
-const mList = document.getElementById("movies-list")
-
-let sortedNames = movieNames.sort();
-//reference
-let input = document.getElementById("movies-list");
-//Execute function on keyup
-input.addEventListener("keyup", (e) => {
-  //loop through above array
-  //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
-  removeElements();
-  for (let i of sortedNames) {
-    //convert input to lowercase and compare with each string
-    if (
-      i.toLowerCase().includes(input.value.toLowerCase()) &&
-      input.value != ""
-    ) {
-      //create li element
-      let listItem = document.createElement("li");
-      //One common class name
-      listItem.classList.add("list-items");
-      listItem.style.cursor = "pointer";
-      listItem.setAttribute("onclick", "displayNames('" + i + "')");
-      //Display matched part in bold
-      let word = "<b>" + i.substring(0, input.value.length) + "</b>";
-      word += i.substring(input.value.length);
-      //display the value in array
-      listItem.innerHTML = word;
-      document.querySelector(".list").appendChild(listItem);
-    }
-  }
-});
-
-
-function displayNames(value) {
-  input.value = value;
-  removeElements();
-}
-function removeElements() {
-  //clear all the item
-  let items = document.querySelectorAll(".list-items");
-  items.forEach((item) => {
-    item.remove();
-  });
-}
+const moviesUrl = "./assets/script/movies.json";
+const citiesUrl = "./assets/script/cities.json";
 
 const options = {
   method: "GET",
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   },
-  mode: "no-cors",
+  mode: "cors",
 };
 
-const movies = "./assets/script/movies.json";
-const cities = "./assets/script/cities.json";
-
-async function getMovies() {
+async function fetchJson(url) {
   try {
-    const response = await fetch(movies, options);
+    const response = await fetch(url, options);
 
-    if (!response.ok)
+    if (!response.ok) {
       throw new Error(`${response.statusText} ${response.status}`);
-
-    const data = await response.json();
-    const grid = select(".grid-container");
-
-    function get(array) {
-      grid.innerHTML = "";
-
-      array.forEach((element) => {
-        grid.innerHTML += ` <div class="grid-item">
-               <div class="poster">
-                 <img
-                   src="${element.img}"
-                   alt="movie-poster"
-                 />
-               </div>
-               <p class="movie-title"><strong>${element.title}</strong><br>(${element.year})</p>
-       `;
-      });
     }
-    get(data.results);
+
+    return await response.json();
   } catch (error) {
     print(error.message);
   }
 }
 
+async function getMovies() {
+  const data = await fetchJson(moviesUrl);
+  const grid = select(".grid-container");
+
+  function getInfo(array) {
+    grid.innerHTML = "";
+
+    array.forEach((element) => {
+      grid.innerHTML += ` <div class="grid-item">
+        <div class="poster">
+          <img src="${element.img}" alt="movie-poster" />
+        </div>
+        <p class="movie-title"><strong>${element.title}</strong><br>(${element.year})</p>
+      </div>`;
+    });
+  }
+  getInfo(data.results);
+
+  function autoInputs(array, input, list) {
+    onEvent(input, "keyup", function () {
+      let inputValue = input.value.toLowerCase();
+      removeDropdown(input, list);
+
+      if (inputValue.length === 0) return;
+
+      list.innerHTML = " ";
+
+      array.forEach((el) => {
+        if (el.title.toLowerCase().startsWith(inputValue)) {
+          list.innerHTML += `<li>${el.title}</li>`;
+        }
+      });
+    });
+  }
+  autoInputs(data.results, moviesInput, listmovies);
+}
+
 getMovies();
+
+function removeDropdown(input, list) {
+  if (input.value.length === 0) list.innerHTML = "";
+}
+
+function onItemClick(e) {
+  e.preventDefault();
+
+  const click = e.target;
+  this.value = click.innerHTML;
+  this.nextElementSibling.innerHTML = "";
+}
+
+onEvent(listmovies, "click", onItemClick.bind(moviesInput));
+onEvent(listCities, "click", onItemClick.bind(cityInput));
+
+async function getCities() {
+  const data = await fetchJson(citiesUrl);
+
+  function autoInput(array, input, list) {
+    onEvent(input, "keyup", function () {
+      let inputValue = input.value.toLowerCase();
+
+      if (inputValue.length === 0) return;
+
+      list.innerHTML = " ";
+
+      array.forEach((el) => {
+        if (el.name.toLowerCase().startsWith(inputValue)) {
+          list.innerHTML += `<li>${el.name}</li>`;
+        }
+      });
+    });
+  }
+  autoInput(data.cities, cityInput, listCities);
+}
+
+getCities();
